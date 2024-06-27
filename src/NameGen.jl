e17ccad5961d5f8945dc6860b4405178239b17bb
mtanneau <mathieu.tanneau@gmail.com>
2024-06-27 14:05:41 -0400

    Basic name generator

--- src/NameGen.jl
+++ src/NameGen.jl
@@ -1,5 +1,22 @@
 module NameGen
 
-greet() = print("Hello World!")
+using Random
+
+using InlineStrings
+
+export randname
+
+# lists of words
+include("adjective.jl")
+include("animal.jl")
+include("color.jl")
+
+randname() = randname(Random.GLOBAL_RNG)
+function randname(rng::AbstractRNG)
+    adjective = rand(rng, ADJECTIVES)
+    color = rand(rng, COLORS)
+    animal = rand(rng, ANIMALS)
+    return "$(adjective)-$(color)-$(animal)"
+end
 
 end # module NameGen
